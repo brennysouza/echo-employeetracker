8c7e59ec085424af942a0687a8fe019f7275c9d9
Brenny Souza <106251815+brennysouza@users.noreply.github.com>
2023-09-13 20:19:19 -0400

    revised schema file

--- db/schema.sql
+++ db/schema.sql
@@ -1,27 +1,31 @@
--- Schema file, below is the folder
-CREATE DATABASE company;
+-- Schema file, below is the database
+DROP DATABASE IF EXISTS company_db;
+CREATE DATABASE company_db;
 
-USE company;
-
+USE company_db;
+-- Departments table
 CREATE TABLE department (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(30)
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30) UNIQUE 
 );
-
+-- Employee roles table
 CREATE TABLE role (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    title VARCHAR(30),
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30) UNIQUE,
     salary DECIMAL(10, 2),
     department_id INT,
-    FOREIGN KEY (department_id) REFERENCES department(id)
+    FOREIGN KEY (department_id) 
+        REFERENCES department(id)
 );
-
+-- Employee names
 CREATE TABLE employee (
-    id INT AUTO_INCREMENT PRIMARY KEY,
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30),
     last_name VARCHAR(30),
     role_id INT,
     manager_id INT,
-    FOREIGN KEY (role_id) REFERENCES role(id),
-    FOREIGN KEY (manager_id) REFERENCES employee(id)
+    FOREIGN KEY (role_id)
+        REFERENCES role(id),
+    FOREIGN KEY (manager_id)
+        REFERENCES employee(id)
 );
