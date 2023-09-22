5bb346043d349b1780c20c354a4c3009a64a3fb8
Brenny Souza <106251815+brennysouza@users.noreply.github.com>
2023-09-22 11:42:56 -0400

    made folder for seeds file

--- server.js
+++ server.js
@@ -1,1 +1,40 @@
 const inquirer = require('inquirer');
+const db = require('./db'); // Import your database connection
+
+// Your application logic here
+
+// Start the application
+init();
+
+function init() {
+  // Use inquirer to prompt the user with options
+  inquirer
+    .prompt([
+      // Define prompts for viewing, adding, and updating data
+    ])
+    .then((answers) => {
+      // Handle user's choice and call appropriate functions
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+}
+
+// Create functions for each user action (viewing, adding, updating)
+
+// Example function to view all departments
+function viewDepartments() {
+  // Query the database and display the results in a formatted table
+}
+
+// Example function to add a department
+function addDepartment() {
+  // Prompt the user for department details and insert them into the database
+}
+
+// Example function to update an employee's role
+function updateEmployeeRole() {
+  // Prompt the user to select an employee and update their role
+}
+
+// Call functions based on user choices
