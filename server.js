3b47f1835ff9a5e50b7e6169407b8618a7bfeb65
Brenny Souza <106251815+brennysouza@users.noreply.github.com>
2023-09-29 20:14:37 -0400

    revised dotenv file and included its info inside server.js file

--- server.js
+++ server.js
@@ -5,8 +5,7 @@ require('dotenv').config();
 
 
 const connection = mysql.createConnection({
-  host: 'localhost',
-  port: 3306,
+  host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
@@ -156,4 +155,4 @@ function updateEmployeeRole() {
     });
 }
 
-// Call functions based on user choices
+
